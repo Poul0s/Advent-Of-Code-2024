5ef10d2e44b348737763586f468176825c6c9231
psalame <psalame@psalame.42angouleme.fr>
2024-12-15 15:18:37 +0100

    d11 clean

--- d11/main.py
+++ d11/main.py
@@ -1,36 +1,13 @@
-# import multiprocessing.managers
-# import threading
-# import multiprocessing
 from functools import lru_cache
 
 def load_input():
 	with open("input.txt") as f:
 		return f.read().split(' ')
 
-# def blink(stones, allow_thread=False, result=None, result_i=0):
-	
-# 	if (allow_thread and len(stones) > 16):
-# 		stones_part = []
-# 		sep = int(len(stones) / 16)
-# 		while len(stones) > 0:
-# 			stones_part.append(stones[:sep])
-# 			stones = stones[sep:]
-# 		threads = []
-# 		threads_result = multiprocessing.Manager().dict()
-# 		for i in range(len(stones_part)):
-# 			t = multiprocessing.Process(target=blink, args=(stones_part[i], False, threads_result, i))
-# 			t.start()
-# 			threads.append(t)
-# 		for i in range(len(threads)):
-# 			threads[i].join()
-# 			stones = stones + threads_result[i]
-# 		return stones
-#		...
-
 @lru_cache
 def blink(stones):
 	i = 0
-	if len(stones) > 4:
+	if len(stones) > 5:
 		res = []
 		pad = int(len(stones) / 4)
 		sep = 0
